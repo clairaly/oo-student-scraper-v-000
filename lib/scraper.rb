63e62be5330784e005049310af46f3cf94c97004
Alyssa Clair <clairaly13@gmail.com>
2017-12-18 18:31:35 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,8 @@ class Scraper
   def self.scrape_index_page(index_url)
     index_doc = Nokogiri::HTML(open(index_url))
     students = []
+    end
+  students
   end
 
   def self.scrape_profile_page(profile_url)
